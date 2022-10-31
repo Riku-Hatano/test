5cacd811c791b1bbb8fab3a2ea86f6abffd54812
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-10-31 09:01:10 -0700

    fixed the bug that happened by unintentional rendering by renew the modalimg

--- src/make_modal/card.tsx
+++ src/make_modal/card.tsx
@@ -5,7 +5,7 @@ const Card = (props: any) => {
     // console.log(props.characters)
     // console.log(props.loading)
     // console.log(props.setValues)
-    console.log(props.characters.length)
+    console.log(props.characters)
     const cardBox: any = []
 
     for (let i = 0 ; i < props.characters.length ; i++) {
@@ -32,10 +32,38 @@ const Card = (props: any) => {
                 className: "parentItem",
                 onClick: () => SearchCard(i, props.characters[i].id, props.setValues)
             }, 
-            monsterItem, flavorItem)
-
+            monsterItem, flavorItem
+        )
             cardBox.push(individualCard)
         }
+    // for (let i = 0 ; i < props.characters.length ; i++) {
+    //     const monsterItem = React.createElement(
+    //         "div",
+    //         {
+    //             className: "monsterItem"
+    //         },
+    //         props.characters[i].name
+    //     )
+    //     const flavorItem = React.createElement(
+    //         "div",
+    //         {
+    //             className: "flavorItem"
+    //         },
+    //         props.characters[i].desc
+    //     )
+    //     // monsterItemとflavorItemをindividualCardを親要素にしてにまとめて、一枚のカードのようにする。
+    //     const individualCard = React.createElement(
+    //         "div",
+    //         {
+    //             key: i,
+    //             id: `individualCard${i}`,
+    //             className: "parentItem",
+    //             onClick: () => SearchCard(i, props.characters[i].id, props.setValues)
+    //         }, 
+    //         monsterItem, flavorItem)
+
+    //         cardBox.push(individualCard)
+    //     }
 
         //individualCardがたくさん入ったchildBoxを出力する。
         const cardList = React.createElement(
@@ -46,12 +74,13 @@ const Card = (props: any) => {
             },
             cardBox 
         )
-        
+
     return (
         <>
         {cardList}
         </>
     )
+
 }
 
 export default Card
--- src/make_modal/searchCard.ts
+++ src/make_modal/searchCard.ts
@@ -1,24 +1,29 @@
 import React from "react"
 import react from "react"
 import "../index.css"
+import ModalWindow from "./modal"
 
 const SearchCard = (i: number, id: number, setValues: react.Dispatch<react.SetStateAction<any>>) => {
     let parent = document.getElementById(`individualCard${i}`) as HTMLDivElement
     parent.classList.add("added")
+    console.log(id)
+    // console.log(parent.id)
+    // console.log(parent.className)
+    // console.log(parent.classList[0])
 
     // モーダルウインドウの画像のsrcを変更する処理
     const imgsrc = `https://storage.googleapis.com/ygoprodeck.com/pics/${id}.jpg`
-    setValues ({
-        modalImage: imgsrc
-    })
+    // setValues ({
+    //     modalImage: imgsrc
+    // })
 
     // モーダルウインドウの表示/非表示を切り替える処理
     const parentModalWindow = document.getElementById("parentModalWindow") as HTMLDivElement
     parentModalWindow.style.display = "inline"
 }
 
-// eraseModalWindow() {
-//     const parentModalWindow = document.getElementById("parentModalWindow") as HTMLDivElement
-//     parentModalWindow.style.display = "none"
-// }
+const eraseModalWindow = () => {
+    const parentModalWindow = document.getElementById("parentModalWindow") as HTMLDivElement
+    parentModalWindow.style.display = "none"
+}
 export default SearchCard
