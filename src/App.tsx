aa16ee46699a112e299bf9d49f22c991ac0721cc
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-10-30 17:38:38 -0700

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Routes, BrowserRouter, Route, Link} from "react-router-dom"
+
+import Monster from './page_tsx/Monster'
+import Spell from './page_tsx/Spell'
+import Archetypes from './page_tsx/Archetypes'
+import Home from './page_tsx/Home'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/monster" element={<Monster />} />
+        <Route path="/spell" element={<Spell />} />
+        {/* <Route path="/archetypes" component={Archetypes} /> */}
+        <Route path="archetypes/:archetype" element={<Archetypes />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/page_tsx/Archetypes.tsx
@@ -0,0 +1,270 @@
+import React, {Component} from "react"
+import react from "react"
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+import Card from "../make_modal_card/card"
+import ModalWindow from "../make_modal_card/modal"
+import { useParams } from "react-router-dom"
+
+const ComponentDidMount = (props: any) => {
+    const { archetype } = useParams<{archetype: string}>() 
+
+    props.props({
+        ...props.values,
+        valuesLoading: true
+    })
+    
+    const characters: Array<any> = []
+    let box: any = []
+    
+    useEffect (() => {
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + archetype)
+        .then((results) => {
+            const d = results.data["data"] as Array<any>
+            characters.push(d);       
+            for (let i = 0 ; i < characters.length ; i++) {
+                for (let j = 0 ; j < characters[i].length ; j++) {
+                    box.push(characters[i][j])
+                    console.log(characters[i][j])
+                }
+            }        
+        })
+        .then(() => {
+            props.props({
+                ...props.values,
+                valuesCharacters: box
+            })
+        })
+        .catch(err => {
+            console.log("err:", err);
+        })
+    }, [])
+
+    return (
+        <div id="parentroot">
+            <meta name="viewport" content="width=divice-width, initial-scale=1.0"></meta>
+            <div>hello world!</div>
+            <div>
+                <Card 
+                characters={props.valuesCharacters}
+                loading={props.valuesLoading}
+                />
+            </div>    
+            <div>
+                <ModalWindow
+                src={props.valuesModalImage}
+                />
+            </div>
+        </div>
+    )
+}
+
+// const ComponentDidMount = (values: any, valuesLoading: boolean, valuesCharacters: any, valuesModalImage: string, props: react.Dispatch<react.SetStateAction<any>>) => {
+//     const { archetype } = useParams<{archetype: string}>() 
+//     console.log(values)
+//     console.log(valuesLoading)
+//     console.log(valuesModalImage)
+//     console.log(valuesCharacters)
+//     console.log(props)
+//     props ({
+//         ...values,
+//         valuesLoading: true
+//     })
+    
+//     const characters: Array<any> = []
+//     let box: any = []
+    
+//     axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + archetype)
+//     .then((results) => {
+//         const d = results.data["data"] as Array<any>
+//         characters.push(d);       
+//         for (let i = 0 ; i < characters.length ; i++) {
+//             for (let j = 0 ; j < characters[i].length ; j++) {
+//                 box.push(characters[i][j])
+//                 console.log(characters[i][j])
+//             }
+//         }        
+//     })
+
+//     .then(() => {
+//         props({
+//             ...values,
+//             valuesCharacters: box
+//         })
+//     })
+//     .catch(err => {
+//         console.log("err:", err);
+//     })
+
+//     return (
+//         <div id="parentroot">
+//             <meta name="viewport" content="width=divice-width, initial-scale=1.0"></meta>
+//             <div>done</div>
+//             <div>
+//                 <Card 
+//                 characters={valuesCharacters}
+//                 loading={valuesLoading}
+//                 // this={this}
+//                 />
+//             </div>    
+//             <div>
+//                 <ModalWindow
+//                 src={valuesModalImage}
+//                 // this={this}
+//                 />
+//             </div>
+//         </div>
+//     )
+// }
+
+
+const Archetypes = () => {
+    const [values, setValues] = useState({
+        loading: true,
+        characters: {},
+        modalImage: "https://storage.googleapis.com/ygoprodeck.com/pics/38955728.jpg",
+    })
+    
+    // ComponentDidMount();
+    // useEffect(() => {
+    //     ComponentDidMount(values, values.loading, values.characters, values.modalImage, setValues);
+    // }, [])
+    const { archetype } = useParams<{archetype: string}>() 
+
+    // setValues ({
+    //     ...values,
+    //     loading: true
+    // })
+    
+    const characters: Array<any> = []
+    let box: any = []
+    
+    useEffect (() => {
+        axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + archetype)
+        .then((results) => {
+            const d = results.data["data"] as Array<any>
+            characters.push(d);       
+            for (let i = 0 ; i < characters.length ; i++) {
+                for (let j = 0 ; j < characters[i].length ; j++) {
+                    box.push(characters[i][j])
+                    console.log(characters[i][j])
+                }
+            }        
+        })
+        .then(() => {
+            setValues ({
+                ...values,
+                characters: box
+            })
+        })
+        .catch(err => {
+            console.log("err:", err);
+        })
+    }, [])
+
+    return (
+        <div id="parentroot">
+            <meta name="viewport" content="width=divice-width, initial-scale=1.0"></meta>
+            <div>
+                <Card 
+                characters={values.characters}
+                loading={values.characters}
+                />
+            </div>    
+            <div>
+                <ModalWindow
+                src={values.modalImage}
+                />
+            </div>
+        </div>
+    )
+    // return (
+    //     <ComponentDidMount
+    //     values = {values}
+    //     valuesLoading = {values.loading}
+    //     valuesCharacters = {values.characters}
+    //     valuesModalImage = {values.modalImage}
+    //     props = {setValues}
+    //     />
+    // )
+}
+
+
+
+
+
+
+
+// const Archetypes = () => {
+//     const [values, setValues] = useState({
+//         loading: true,
+//         characters: {},
+//         modalImage: "https://storage.googleapis.com/ygoprodeck.com/pics/38955728.jpg",
+//     })
+
+//     const ComponentDidMount = () => {
+//         const { archetype } = useParams<{archetype: string}>() 
+//         setValues({
+//             ...values,
+//             loading: true
+//         })
+        
+//         const characters: Array<any> = []
+//         let box: any = []
+        
+//         axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=" + archetype)
+//         .then((results) => {
+//             const d = results.data["data"] as Array<any>
+//             characters.push(d);       
+//             for (let i = 0 ; i < characters.length ; i++) {
+//                 for (let j = 0 ; j < characters[i].length ; j++) {
+//                     box.push(characters[i][j])
+//                     console.log(characters[i][j])
+//                 }
+//             }        
+//         })
+    
+//         .then(() => {
+//             setValues({
+//                 ...values,
+//                 characters: box
+//             })
+//         })
+//         .catch(err => {
+//             console.log("err:", err);
+//         })
+//     }
+    
+    
+//     // ComponentDidMount();
+//     useEffect(() => {
+//         ComponentDidMount();
+//     }, [])
+
+//     return (
+//         <div id="parentroot">
+//             <meta name="viewport" content="width=divice-width, initial-scale=1.0"></meta>
+//             <div>done</div>
+//             <div>
+//                 <Card 
+//                 characters={values.characters}
+//                 loading={values.loading}
+//                 // this={this}
+//                 />
+//             </div>    
+//             <div>
+//                 <ModalWindow
+//                 src={values.modalImage}
+//                 // this={this}
+//                 />
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
+
+
+export default Archetypes
