013b1fcefe9938b89b9efbfabc7fbb33ac4321a2
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-11-06 23:15:14 -0800

    不要なコードを削除しました。細かい変更を加えました

--- src/page_tsx/Home.tsx
+++ src/page_tsx/Home.tsx
@@ -3,7 +3,6 @@ import "../page_css/Home.css"
 const Home = () => {
     return (
         <div>
-            <p>done</p>
             <head>
                 <meta charSet="utf-8"></meta>
                 <title>fetch data from Yu-Gi-Oh Api</title>
@@ -20,12 +19,27 @@ const Home = () => {
                 <a href="/archetypes/Albaz Dragon">Albaz Dragon</a> |
                 <a href="/archetypes/Despia">Despia</a> |
                 <a href="/archetypes/Zoodiac">Zoodiac</a> |
-                <a href="/archetypes/Keldo">Keldo</a>
             </div>
         </div>
     )
 }
 
+
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8" />
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Document</title>
+// </head>
+// <body>
+    
+// </body>
+// </html>
 
 
 export default Home
